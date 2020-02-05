495945efec219a3246f907834f500854a89af7ed
Nico De Witte <nico.dewitte@outlook.com>
2020-02-05 19:53:47 +0100

    Configure vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: 'Object Oriented Programming with C++',
+  description: 'C++ Programming Course for VIVES University of Applied Sciences (Bachelor Degree)',
+  themeConfig: {
+    nav: [
+    ],
+    sidebar: [
+        '01_hello_world/',
+    ],
+    sidebarDepth: 1,
+    repo: 'BioBoost/course-object-oriented-programming-with-cpp',
+    docsDir: 'docs',
+    docsBranch: 'master'
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  serviceWorker: true,
+  plugins: [
+    ['vuepress-plugin-zooming', {
+      // selector for images that you want to be zoomable
+      // default: '.content img'
+      selector: 'img',
+
+      // make images zoomable with delay after entering a page
+      // default: 500
+      // delay: 1000,
+
+      // options of zooming
+      // default: {}
+      options: {
+        bgColor: 'black',
+        zIndex: 10000,
+      },
+    }],
+    ['container', {
+      type: 'codeoutput',
+      defaultTitle: 'Output',
+    }]
+  ],
+}
