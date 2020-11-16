3468aac4a8bf1df84cab71b47aa05e82b77e7e90
Nico De Witte <nico.dewitte@outlook.com>
2020-11-16 08:52:26 +0100

    Add chapter on makefiles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -73,7 +73,7 @@ module.exports = {
         sidebarDepth: 1,    // optional, defaults to 1
         children: [
           '/e-compiling-and-linking/01-the-compilation-process/',
-          // '/e-compiling-and-linking/02-makefiles/',
+          '/e-compiling-and-linking/02-makefiles/',
           // '/e-compiling-and-linking/03-libraries/'
         ]
       },
